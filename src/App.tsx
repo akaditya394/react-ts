5dc85bb2856df45901910e99e4241c34ae5b0267
Aditya <akaditya394@gmail.com>
2022-05-07 11:44:34 +0530

    todos component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import Todos from "./component/Todos";
 
 function App() {
   return (
-    <div>App</div>
+    <Todos items={["Learn React", "Learn typescript"]} />
   )
 }
 
